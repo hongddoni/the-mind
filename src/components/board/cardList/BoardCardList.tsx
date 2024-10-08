b340fe46a0fdf4ea41e749fcd8bbb1a3f9abd939
hongddoni <hongseunghyeok@Hongui-MacBookPro.local>
2024-10-08 21:57:55 +0900

    빌드오류 수정

--- src/components/board/cardList/BoardCardList.tsx
+++ src/components/board/cardList/BoardCardList.tsx
@@ -3,13 +3,12 @@ import { Card } from "../../card/Card.tsx";
 import s from "./boardCardList.module.scss";
 import { BoardCardWrap } from "./BoardCardWrap.tsx";
 import { useSocketContext } from "../../container/socket/SocketProvider.tsx";
-import {Button} from "../../button/Button.tsx";
-import {Label} from "../../label/Label.tsx";
+import { Button } from "../../button/Button.tsx";
+import { Label } from "../../label/Label.tsx";
 
 export const BoardCardList = () => {
-	const { cards, ref, maxWidth } = useBoardCardList();
+	const { cards, ref } = useBoardCardList();
 	const { isGameAvailable, onReady, users } = useSocketContext()!;
-
 
 	const renderer = () => {
 		if (!isGameAvailable) {
@@ -18,10 +17,14 @@ export const BoardCardList = () => {
 					<div className={s.info}>
 						<span className={s.description}>현재 참여자</span>
 						<div className={s.participants}>
-							{users?.map((v) => <Label>{v.nickname}</Label>)}
+							{users?.map((v) => (
+								<Label>{v.nickname}</Label>
+							))}
 						</div>
 					</div>
-					<Button onClick={onReady} block>게임 준비</Button>
+					<Button onClick={onReady} block>
+						게임 준비
+					</Button>
 				</div>
 			);
 		}
@@ -30,16 +33,16 @@ export const BoardCardList = () => {
 			<div className={s.cardWrap}>
 				{cards?.map((card, index) => {
 					return (
-                        <BoardCardWrap
-                            key={card}
-                            zIndex={index}
-                            x={Math.floor(Math.random() * 11) - 5}
-                            y={Math.floor(Math.random() * 11) - 5}
-                        >
-                            <Card number={card} hightLight={false} />
-                        </BoardCardWrap>
-					)
-                })}
+						<BoardCardWrap
+							key={card}
+							zIndex={index}
+							x={Math.floor(Math.random() * 11) - 5}
+							y={Math.floor(Math.random() * 11) - 5}
+						>
+							<Card number={card} hightLight={false} />
+						</BoardCardWrap>
+					);
+				})}
 			</div>
 		);
 	};
