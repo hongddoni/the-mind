4aba0428c7560d450de7064f58cf62aeec6a8541
hongddoni <hongseunghyeok@Hongui-MacBookPro.local>
2024-10-10 13:28:45 +0800

    더마인드 분리, 차우차우 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { User } from "./components/user/view/User.tsx";
-import { BoardContainer } from "./components/container/board/BoardContainer.tsx";
-import { BoardCardList } from "./components/board/cardList/BoardCardList.tsx";
-import { Dashboard } from "./components/dashboard/Dashboard.tsx";
-import { GameOverModal } from "./components/modal/GameOverModal.tsx";
-import { SocketProvider } from "./components/container/socket/SocketProvider.tsx";
-import { Login } from "./components/login/Login.tsx";
-import { useState } from "react";
-import { MainContainer } from "./components/container/main/MainContainer.tsx";
+import { ChaoChao } from "./\bchaochao/ChaoChao";
+import { ChaoChaoProvier } from "./\bchaochao/ChaoChaoProvider";
+import { TheMind } from "./themind/TheMind";
 
 function App() {
-	const [open, setOpen] = useState<boolean>(true);
-	return (
-		<SocketProvider>
-			<MainContainer>
-				{open && <Login onClose={() => setOpen(false)} />}
-				<Dashboard />
-				<BoardContainer>
-					<BoardCardList />
-				</BoardContainer>
-				<User />
-				{/* <ChatButton /> */}
-				<GameOverModal />
-			</MainContainer>
-		</SocketProvider>
-	);
+	return <TheMind />;
 }
 
 export default App;
